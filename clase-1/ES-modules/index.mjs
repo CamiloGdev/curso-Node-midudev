d273c1304b298d1a3ebb0df690b75d61f68c0a9f
CamiloGdev <chcamilog@gmail.com>
2023-08-01 23:00:36 -0500

    feat: uso de ES Modules
    pequeño modulo de operaciones matematicas haciendo uso de ES Modules

---
+++ clase-1/ES-modules/index.mjs
@@ -0,0 +1,6 @@
+import { sum, sub, mult, div } from "./operations.mjs"
+
+console.log(sum(1, 2))
+console.log(sub(1, 2))
+console.log(mult(1, 2))
+console.log(div(1, 2))
--- clase-1/cjs/index.js
+++
@@ -1,6 +0,0 @@
-//CommonJS require module
-// const sum = require('./sum');
-const { rest, sum } = require('./sum');
-
-console.log(sum(1, 2));
-console.log(rest(1, 2));
