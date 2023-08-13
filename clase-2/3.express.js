71fafe0aabdb67435573d48f0003bb4b641a78bf
CamiloGdev <chcamilog@gmail.com>
2023-08-13 00:47:16 -0500

    feat: implementación de middelware
    procesamos la request por medio de un middelware para capturar el body y
    parsearlo, extrayendo esa logica del manejo de la peticon POST

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -7,12 +7,15 @@ const PORT = process.env.PORT ?? 3000
 
 app.disable('x-powered-by') // <-- desactivación de cabecera para no enviar la herramienta con la que se esta trabajando
 
-// con express en lugar de definir una funcion que procesa las request, contamos con metodos indendientes basados en los diferentes metodos HTTP
-app.get('/pokemon/ditto', (req, res) => {
-  res.status(200).json(dittoJSON) // <-- con express la res tiene una funcionalidad mas especial
-})
+// Middelware para todas las peticiones que entren
+app.use((req, res, next) => {
+  // podriamos trackear la request a la base de datos
+  // revisar si el usuario tiene cookies
+  // por ekemplo podemos traernos la logica que toma el cuerpo de la request y la almacena como JSON en una variable
+  if (req.method !== 'POST') return next()
+  if (req.headers['content-type'] !== 'application/json') return next()
 
-app.post('/pokemon', (req, res) => {
+  // aca solo llegan request que son POST y que tineen el header Content-Type: application/json
   let body = ''
 
   // Escuchar el evento data de la request
@@ -22,12 +25,21 @@ app.post('/pokemon', (req, res) => {
 
   req.on('end', () => {
     const data = JSON.parse(body)
-    // aca podriamos llamar a la base de datos para procezar data
-
     data.timestamp = Date.now()
-
-    res.status(201).send(data)
+    // mutar la request y meter la informacion parseada en el req.body
+    req.body = data
+    next()
   })
+})
+
+// con express en lugar de definir una funcion que procesa las request, contamos con metodos indendientes basados en los diferentes metodos HTTP
+app.get('/pokemon/ditto', (req, res) => {
+  res.status(200).json(dittoJSON) // <-- con express la res tiene una funcionalidad mas especial
+})
+
+app.post('/pokemon', (req, res) => {
+  // aca podriamos guardar en DB con el req.body
+  res.status(201).json(req.body)
 })
 
 // para manejar los errores en la petición establecemos un tratamiento global de las request, pero al final para garantizar si ya se ha dado una peticion establecida
