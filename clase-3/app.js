78cb9e3941d7ff55db6a5d1a4bd229520fc8dddc
CamiloGdev <chcamilog@gmail.com>
2023-08-18 03:22:11 -0500

    feat(api movies): corrección de errores de cors
    página para visualización y eliminación de recurso movies,
    agregación de cabeceras de
    orígenes y métodos
    permitidos, para las respuestas a get y delete para corregir
    errores de cors por peticiones provenientes de dominios diferentes al
    de la api.

--- clase-3/app.js
+++ clase-3/app.js
@@ -8,11 +8,25 @@ const app = express()
 app.use(express.json())
 app.disable('x-powered-by')
 
+const ACEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://movies.com',
+  'http://camilo.dev',
+]
+
 // Todos los recursos que sean MOVIES se indentifican con /movies
 
 // OBTENER TODAS LA MOVIES
 // CON OPCIÓN DE FILTRAR POR GENRE
 app.get('/movies', (req, res) => {
+  // Añadimos los permisos de CORS para poder realisar la request solo a este endpoint desde un dominio diferente al de nuestra API
+  const origin = req.header('origin') // <-- traemos el dominion de origen de la request
+
+  // Se va a añadir la cabecera solo en el caso en que el dominio coincida con uno de los aceptados o que este vacio, vacío tambíen se acepta, ya que este solo esta vacío en el caso en que la request se realice desde el mismo dominio de nuestra API
+  if (ACEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin || 'http://localhost:3000') // <-- debemos añadir este header para permitir a otros dominios acceder
+  }
+
   const { genre } = req.query
 
   if (genre) {
@@ -56,6 +70,11 @@ app.post('/movies', (req, res) => {
 
 // ELIMINAR UNA MOVIE
 app.delete('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+  if (ACEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin || 'http://localhost:3000')
+  }
+
   const { id } = req.params
   console.log(id)
   const movieIndex = movies.findIndex((movie) => movie.id === id)
@@ -92,6 +111,16 @@ app.patch('/movie/:id', (req, res) => {
   movies[movieIndex] = updateMovie
 
   return res.json(updateMovie)
+})
+
+app.options('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+
+  if (ACEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+  }
+  res.sendStatus(200)
 })
 
 const PORT = process.env.PORT ?? 3000
