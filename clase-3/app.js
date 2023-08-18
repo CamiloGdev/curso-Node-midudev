cafd9ff04f6840649ba7a3914da64eb2c6ea248e
CamiloGdev <chcamilog@gmail.com>
2023-08-18 02:53:11 -0500

    feat(api movies): procesamiento de get request
    obtener todas las movies,
    obtener movies filtrando por género,
    obtener una movie por su id.

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,13 +1,39 @@
 const express = require('express')
+const crypto = require('node:crypto')
+
+const movies = require('./movies.json')
 
 const app = express()
-const PORT = process.env.PORT ?? 3000
-
+app.use(express.json())
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.json({ message: 'hola mundo' })
+// Todos los recursos que sean MOVIES se indentifican con /movies
+
+// OBTENER TODAS LA MOVIES
+// CON OPCIÓN DE FILTRAR POR GENRE
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  return res.json(movies)
 })
+
+// OBTENER UNA MOVIE POR SU ID
+app.get('/movies/:id', (req, res) => {
+  // path-to-regex
+  const { id } = req.params
+  const movie = movies.find((movie) => movie.id === id)
+  if (movie) return res.json(movie)
+
+  return res.json({ message: 'Movie not found' })
+})
+
+const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () => {
   console.log(`server listening on port: http://localhost:${PORT}`)
