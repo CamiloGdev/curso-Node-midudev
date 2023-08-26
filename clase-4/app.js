2adea8e7eb723fe9f353187778e2d7c96875d19d
CamiloGdev <chcamilog@gmail.com>
2023-08-26 15:02:52 -0500

    feat: implementación de mvc
    inicio implementación de arquitectura mvc en la api de películas.

---
+++ clase-4/app.js
@@ -0,0 +1,121 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const cors = require('cors')
+
+const movies = require('./movies.json')
+const { validateMovie, validatePartialMovie } = require('./schemas/movies.js')
+
+const app = express()
+app.use(express.json())
+app.disable('x-powered-by')
+
+// Middelware para solucionar los errores de cors
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACEPTED_ORIGINS = [
+        'http://localhost:8080',
+        'http://movies.com',
+        'http://camilo.dev',
+      ]
+
+      if (ACEPTED_ORIGINS.includes(origin) || !origin) {
+        return callback(null, true)
+      }
+
+      return callback(new Error('Not allowed by CORS'))
+    },
+  })
+)
+
+// Todos los recursos que sean MOVIES se identifican con /movies
+
+// OBTENER TODAS LA MOVIES
+// CON OPCIÓN DE FILTRAR POR GENRE
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
+  return res.json(movies)
+})
+
+// OBTENER UNA MOVIE POR SU ID
+app.get('/movies/:id', (req, res) => {
+  // path-to-regex
+  const { id } = req.params
+  const movie = movies.find((movie) => movie.id === id)
+  if (movie) return res.json(movie)
+
+  return res.json({ message: 'Movie not found' })
+})
+
+// CREAR UNA NUEVA MOVIE
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    // <-- también podemos verificar el parámetro contrario con (!result.success)
+    return res.status(400).json({ error: JSON.parse(result.error.message) }) // <-- también podría usarse el código 422 (Unprocessable Entity)
+  }
+
+  // Esto se debe hacer en DB
+  const newMovie = {
+    id: crypto.randomUUID(), // <-- generamos un uuid v4 usando el modulo nativo crypto
+    ...result.data,
+  }
+
+  movies.push(newMovie) // <-- no seria REST por que estamos guardando el estado (la data) de la app en memoria en la variable movies, pero esto se resuelve guardando en base de datos mas adelante
+
+  return res.status(201).json(newMovie)
+})
+
+// ELIMINAR UNA MOVIE
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  console.log(id)
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ error: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
+})
+
+// ACTUALIZAR PARCIALMENTE UNA MOVIE
+app.patch('/movie/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(422).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex((movie) => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(400).json({ error: 'Movie not found' })
+  }
+
+  // Esto se debe hacer en DB
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data,
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 3000
+
+app.listen(PORT, () => {
+  console.log(`server listening on port: http://localhost:${PORT}`)
+})
---
+++ clase-4/schemas/movies.js
@@ -0,0 +1,48 @@
+// creamos un esquema con zod para la validación de los datos para las movies
+
+const z = require('zod') // <-- paquete para realizar validaciones
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string.',
+    required_error: 'Movie title is required.',
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().min(0),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL',
+  }),
+  genre: z.array(
+    z.enum([
+      'Action',
+      'Adventure',
+      'Comedy',
+      'Drama',
+      'Fantasy',
+      'Horror',
+      'Thriller',
+      'Sci-Fi',
+      'Crime',
+    ]),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enum Genre.',
+    }
+  ),
+})
+
+// Validamos todos los atributos del recurso
+function validateMovie(input) {
+  return movieSchema.safeParse(input)
+}
+
+// Agregando el método partial() a la validación, la realizamos parcialmente, cada validación se convierte en opcional, ya que solo nos pasaran algunos de los atributos cuando queramos por ejemplo actualizar parcialmente el recurso
+function validatePartialMovie(input) {
+  return movieSchema.partial().safeParse(input)
+}
+module.exports = {
+  validateMovie,
+  validatePartialMovie,
+}
