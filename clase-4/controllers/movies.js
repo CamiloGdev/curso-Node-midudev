6b86705b9d8df1be64f546b9282b2811800e3a00
CamiloGdev <chcamilog@gmail.com>
2023-08-28 23:03:15 -0500

    feat(mvc): modularización de controller movies
    modularización de los métodos de control de las peticiones en el
    controlador moviecontroller

---
+++ clase-4/controllers/movies.js
@@ -0,0 +1,69 @@
+import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
+import { MovieModel } from '../models/movie.js'
+
+export class MovieController {
+  // OBTENER TODAS LA MOVIES
+  // CON OPCIÓN DE FILTRAR POR GENRE
+  static async getAll(req, res) {
+    const { genre } = req.query
+
+    const movies = await MovieModel.getAll({ genre })
+    return res.json(movies)
+  }
+
+  // OBTENER UNA MOVIE POR SU ID
+  static async getById(req, res) {
+    // path-to-regex
+    const { id } = req.params
+    const foundMovie = await MovieModel.getById({ id })
+    if (foundMovie) return res.json(foundMovie)
+
+    return res.json({ message: 'Movie not found' })
+  }
+
+  // CREAR UNA NUEVA MOVIE
+  static async create(req, res) {
+    const result = validateMovie(req.body)
+
+    if (result.error) {
+      // <-- también podemos verificar el parámetro contrario con (!result.success)
+      return res.status(400).json({ error: JSON.parse(result.error.message) }) // <-- también podría usarse el código 422 (Unprocessable Entity)
+    }
+
+    const newMovie = await MovieModel.createMovie({ input: result.data })
+
+    return res.status(201).json(newMovie)
+  }
+
+  // ELIMINAR UNA MOVIE
+  static async delete(req, res) {
+    const { id } = req.params
+    const deletedMovie = await MovieModel.deleteMovie({ id })
+
+    if (deletedMovie === false) {
+      return res.status(404).json({ error: 'Movie not found' })
+    }
+
+    return res.json({ message: 'Movie deleted' })
+  }
+
+  // ACTUALIZAR PARCIALMENTE UNA MOVIE
+  static async update(req, res) {
+    const result = validatePartialMovie(req.body)
+
+    if (!result.success) {
+      return res.status(422).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const { id } = req.params
+    const updatedMovie = await MovieModel.updateMovie({
+      id,
+      input: result.data,
+    })
+    if (updatedMovie === false) {
+      return res.status(400).json({ error: 'Movie not found' })
+    }
+
+    return res.json(updatedMovie)
+  }
+}
--- clase-4/routes/movies.js
+++ clase-4/routes/movies.js
@@ -1,69 +1,22 @@
 // con este enrrutador direccionaremos todas las peticiones a las rutas que empiecen con ../movies
 
 import { Router } from 'express'
-import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
-import { MovieModel } from '../models/movie.js'
+import { MovieController } from '../controllers/movies.js'
 
 export const moviesRouter = Router()
 
 // OBTENER TODAS LA MOVIES
 // CON OPCIÓN DE FILTRAR POR GENRE
-moviesRouter.get('/', async (req, res) => {
-  const { genre } = req.query
-
-  const movies = await MovieModel.getAll({ genre })
-  return res.json(movies)
-})
+moviesRouter.get('/', MovieController.getAll)
 
 // OBTENER UNA MOVIE POR SU ID
-moviesRouter.get('/:id', async (req, res) => {
-  // path-to-regex
-  const { id } = req.params
-  const foundMovie = await MovieModel.getById({ id })
-  if (foundMovie) return res.json(foundMovie)
-
-  return res.json({ message: 'Movie not found' })
-})
+moviesRouter.get('/:id', MovieController.getById)
 
 // CREAR UNA NUEVA MOVIE
-moviesRouter.post('/', async (req, res) => {
-  const result = validateMovie(req.body)
-
-  if (result.error) {
-    // <-- también podemos verificar el parámetro contrario con (!result.success)
-    return res.status(400).json({ error: JSON.parse(result.error.message) }) // <-- también podría usarse el código 422 (Unprocessable Entity)
-  }
-
-  const newMovie = await MovieModel.createMovie({ input: result.data })
-
-  return res.status(201).json(newMovie)
-})
+moviesRouter.post('/', MovieController.create)
 
 // ELIMINAR UNA MOVIE
-moviesRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-  const deletedMovie = await MovieModel.deleteMovie({ id })
-
-  if (deletedMovie === false) {
-    return res.status(404).json({ error: 'Movie not found' })
-  }
-
-  return res.json({ message: 'Movie deleted' })
-})
+moviesRouter.delete('/:id', MovieController.delete)
 
 // ACTUALIZAR PARCIALMENTE UNA MOVIE
-moviesRouter.patch('/:id', async (req, res) => {
-  const result = validatePartialMovie(req.body)
-
-  if (!result.success) {
-    return res.status(422).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-  const updatedMovie = await MovieModel.updateMovie({ id, input: result.data })
-  if (updatedMovie === false) {
-    return res.status(400).json({ error: 'Movie not found' })
-  }
-
-  return res.json(updatedMovie)
-})
+moviesRouter.patch('/:id', MovieController.update)
