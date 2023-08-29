87acfdc0d3f3aad345d37b554dfdab33d5886aaf
CamiloGdev <chcamilog@gmail.com>
2023-08-28 22:24:16 -0500

    feat(mvc): modularización de modelo movie
    separación de los métodos que manejan internamente los datos
    en el modelo MovieModel

---
+++ clase-4/models/movie.js
@@ -0,0 +1,70 @@
+// con el modelo movie realizamos el manejo interno de los datos, las reglas del negocio
+
+import { randomUUID } from 'node:crypto'
+
+import { readJSON } from '../utils.js' // <-- importamos el modulo  que creamos para leer los json como si hicieramos un requiere
+const movies = readJSON('./movies.json') // usando el reasJSON importado ahora si podemos importar directamente nuestro json como si usaramos CommonJS
+
+export class MovieModel {
+  // OBTENER TODAS LA MOVIES
+  // CON OPCIÓN DE FILTRAR POR GENRE
+  static getAll({ genre }) {
+    if (genre) {
+      const filteredMovies = movies.filter((movie) =>
+        movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+      )
+      return filteredMovies
+    }
+    return movies
+  }
+
+  // OBTENER UNA MOVIE POR SU ID
+  static getById({ id }) {
+    const movieById = movies.find((movie) => movie.id === id)
+    if (movieById) return movieById
+
+    return false
+  }
+
+  // CREAR UNA NUEVA MOVIE
+  static createMovie({ input }) {
+    // Esto se debe hacer en DB
+    const newMovie = {
+      id: randomUUID(), // <-- generamos un uuid v4 usando el modulo nativo crypto
+      ...input,
+    }
+
+    movies.push(newMovie) // <-- no seria REST por que estamos guardando el estado (la data) de la app en memoria en la variable movies, pero esto se resuelve guardando en base de datos mas adelante
+
+    return newMovie
+  }
+
+  // ELIMINAR UNA MOVIE
+  static deleteMovie({ id }) {
+    const movieIndex = movies.findIndex((movie) => movie.id === id)
+
+    if (movieIndex === -1) {
+      return false
+    }
+
+    movies.splice(movieIndex, 1)
+
+    return true
+  }
+
+  // ACTUALIZAR PARCIALMENTE UNA MOVIE
+  static updateMovie({ id, input }) {
+    const movieIndex = movies.findIndex((movie) => movie.id === id)
+    if (movieIndex === -1) {
+      return false
+    }
+
+    // Esto se debe hacer en DB
+    movies[movieIndex] = {
+      ...movies[movieIndex],
+      ...input,
+    }
+
+    return movies[movieIndex]
+  }
+}
--- clase-4/routes/movies.js
+++ clase-4/routes/movies.js
@@ -1,41 +1,32 @@
 // con este enrrutador direccionaremos todas las peticiones a las rutas que empiecen con ../movies
 
 import { Router } from 'express'
-import { randomUUID } from 'node:crypto'
 import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
-
-import { readJSON } from '../utils.js' // <-- importamos el modulo  que creamos para leer los json como si hicieramos un requiere
-const movies = readJSON('./movies.json') // usando el reasJSON importado ahora si podemos importar directamente nuestro json como si usaramos CommonJS
+import { MovieModel } from '../models/movie.js'
 
 export const moviesRouter = Router()
-console.log(moviesRouter)
 
 // OBTENER TODAS LA MOVIES
 // CON OPCIÓN DE FILTRAR POR GENRE
-moviesRouter.get('/', (req, res) => {
+moviesRouter.get('/', async (req, res) => {
   const { genre } = req.query
 
-  if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
-    )
-    return res.json(filteredMovies)
-  }
+  const movies = await MovieModel.getAll({ genre })
   return res.json(movies)
 })
 
 // OBTENER UNA MOVIE POR SU ID
-moviesRouter.get('/:id', (req, res) => {
+moviesRouter.get('/:id', async (req, res) => {
   // path-to-regex
   const { id } = req.params
-  const movie = movies.find((movie) => movie.id === id)
-  if (movie) return res.json(movie)
+  const foundMovie = await MovieModel.getById({ id })
+  if (foundMovie) return res.json(foundMovie)
 
   return res.json({ message: 'Movie not found' })
 })
 
 // CREAR UNA NUEVA MOVIE
-moviesRouter.post('/', (req, res) => {
+moviesRouter.post('/', async (req, res) => {
   const result = validateMovie(req.body)
 
   if (result.error) {
@@ -43,34 +34,25 @@ moviesRouter.post('/', (req, res) => {
     return res.status(400).json({ error: JSON.parse(result.error.message) }) // <-- también podría usarse el código 422 (Unprocessable Entity)
   }
 
-  // Esto se debe hacer en DB
-  const newMovie = {
-    id: randomUUID(), // <-- generamos un uuid v4 usando el modulo nativo crypto
-    ...result.data,
-  }
-
-  movies.push(newMovie) // <-- no seria REST por que estamos guardando el estado (la data) de la app en memoria en la variable movies, pero esto se resuelve guardando en base de datos mas adelante
+  const newMovie = await MovieModel.createMovie({ input: result.data })
 
   return res.status(201).json(newMovie)
 })
 
 // ELIMINAR UNA MOVIE
-moviesRouter.delete('/:id', (req, res) => {
+moviesRouter.delete('/:id', async (req, res) => {
   const { id } = req.params
-  console.log(id)
-  const movieIndex = movies.findIndex((movie) => movie.id === id)
+  const deletedMovie = await MovieModel.deleteMovie({ id })
 
-  if (movieIndex === -1) {
+  if (deletedMovie === false) {
     return res.status(404).json({ error: 'Movie not found' })
   }
-
-  movies.splice(movieIndex, 1)
 
   return res.json({ message: 'Movie deleted' })
 })
 
 // ACTUALIZAR PARCIALMENTE UNA MOVIE
-moviesRouter.patch('/:id', (req, res) => {
+moviesRouter.patch('/:id', async (req, res) => {
   const result = validatePartialMovie(req.body)
 
   if (!result.success) {
@@ -78,18 +60,10 @@ moviesRouter.patch('/:id', (req, res) => {
   }
 
   const { id } = req.params
-  const movieIndex = movies.findIndex((movie) => movie.id === id)
-  if (movieIndex === -1) {
+  const updatedMovie = await MovieModel.updateMovie({ id, input: result.data })
+  if (updatedMovie === false) {
     return res.status(400).json({ error: 'Movie not found' })
   }
 
-  // Esto se debe hacer en DB
-  const updateMovie = {
-    ...movies[movieIndex],
-    ...result.data,
-  }
-
-  movies[movieIndex] = updateMovie
-
-  return res.json(updateMovie)
+  return res.json(updatedMovie)
 })
